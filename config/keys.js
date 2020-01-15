9860a8a0d7aa7ca91c968ff9d0b892dd97cf8e00
Alvine <alvine@tamkin.se>
2020-01-15 15:52:54 +0100

    Minor spelling mistake

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 // Figure out what set of credentials to return
 if(process.env.NODE_ENV === 'production') {
   // In production
-  module.exports = requore('./prod');
+  module.exports = require('./prod');
 } else {
   // In dev
   module.exports = require('./dev');
