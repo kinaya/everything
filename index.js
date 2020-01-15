5b2d925cec03d8d627a212420abe3db3fd2a6281
Alvine <alvine@tamkin.se>
2020-01-15 11:31:49 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// CommonJS modules. "Import express from 'express'" not supported in node!
+const express = require('express');
+
+// Generate a running express app
+const app = express();
+
+// calling .get creates a route handler. Express calls the function
+app.get('/', (req, res) => {
+  res.send({hi: 'there'}); // Close request and send body
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT); // Express tells node to listen to port 5000
