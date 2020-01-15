b36a64052447bc61d026c7be5ba2006cdf08cd66
Alvine <alvine@tamkin.se>
2020-01-15 15:59:26 +0100

    Google strategy settings

--- services/passport.js
+++ services/passport.js
@@ -22,7 +22,8 @@ passport.deserializeUser((id, done) => {
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
+    callbackURL: '/auth/google/callback',
+    proxy: true
   }, (accessToken, refreshToken, profile, done) => {
     //console.log('profile', profile);
     User.findOne({googleId: profile.id}).then((existingUser) => {
