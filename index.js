5eae7a7afb7fe5e147cc12a5a6d16e03e62cf3b4
Alvine <alvine@tamkin.se>
2020-01-19 14:38:20 +0100

    Spelling bug

--- index.js
+++ index.js
@@ -43,9 +43,9 @@ if(process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
   // express will server index.html if it doesnt recognise the route
   const path = require('path')
-  app.get('*'), (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
-  }
+  });
 }
 
 const PORT = process.env.PORT || 5000;
